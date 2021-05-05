311005893a2715c386ed27aefb754aadc2aa1e76
Alfredo Hugueth <alfredohugueth99@gmail.com>
2021-05-05 07:19:33 -0500

    Section 10-104

---
+++ src/models/usuario.model.ts
@@ -0,0 +1,46 @@
+import { Schema, model } from "mongoose";
+
+
+const UsuarioSchema = new Schema({
+    nombre: {
+
+        type:String,
+        required: true,
+        
+    },
+    email: {
+
+        type:String,
+        required: true,
+        unique:true
+
+    },
+    password: {
+
+        type:String,
+        required: true,
+
+    },
+    img: {
+
+        type:String,
+
+    },
+    role: {
+
+        type:String,
+        required: true,
+        default: 'USER_ROLE'
+
+    },
+    google: {
+        
+        type:Boolean,
+        default: false
+
+    },
+    
+
+});
+
+module.exports = model( 'Usuario', UsuarioSchema);
